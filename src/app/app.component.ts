aa001cf3293d03fbd4604dc4a818d735070e8396
Zack <SimplyComplexable@gmail.com>
2017-06-22 00:11:42 -0600

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {VibrationPage} from "../pages/vibration/vibration";
+import {GeolocationPage} from "../pages/geolocation/geolocation";
+import {CameraPage} from "../pages/camera/camera";
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Vibration', component: VibrationPage },
+      { title: 'Geolocation', component: GeolocationPage },
+      { title: 'Camera', component: CameraPage }
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {VibrationPage} from "../pages/vibration/vibration";
+import {Vibration} from "@ionic-native/vibration";
+import {GeolocationPage} from "../pages/geolocation/geolocation";
+import {Geolocation} from "@ionic-native/geolocation";
+import {CameraPage} from "../pages/camera/camera";
+import {Camera} from "@ionic-native/camera";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    VibrationPage,
+    GeolocationPage,
+    CameraPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +30,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    VibrationPage,
+    GeolocationPage,
+    CameraPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Vibration,
+    Geolocation,
+    Camera
   ]
 })
 export class AppModule {}
